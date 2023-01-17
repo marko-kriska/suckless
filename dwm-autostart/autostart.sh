3afff272032641e0f17d958d65c5edfea4ee909a
Marko Kriska <marko.kriska@pm.me>
2023-01-17 16:14:08 +0100

    update

--- dwm-autostart/autostart.sh
+++ dwm-autostart/autostart.sh
@@ -1,3 +1,4 @@
 # Autostart section
 xrandr --output eDP1 --mode 1366x768 --pos 1920x0 --output HDMI2 --primary --mode 1920x1080 --pos 0x0
+xrandr --output HDMI2 --set "Broadcast RGB" "Full"
 xbanish &
--- dwm-autostart/config.def.h
+++ dwm-autostart/config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code Nerd Font:weight=medium:size=10" };
-static const char dmenufont[]       = "Fira Code Nerd Font:weight=medium:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -35,10 +35,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster        = 1;    /* number of clients in master area */
-static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -64,7 +64,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray1, "-sf", col_cyan, NULL };
 static const char *termcmd[]    = { "alacritty", NULL };
 static const char *mixer[]      = { "alacritty", "--class", "pulsemixer,pulsemixer", "-e", "pulsemixer", NULL };
-static const char *filecmd[]    = { "nemo", NULL };
+static const char *rss[]        = { "alacritty", "--class", "newsboat,newsboat", "-e", "newsboat", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *screenshot[] = { "scrot", "-s", "-e", "mv $f ~/Downloads", NULL };
 static const char *passcmd[]    = { "rofi-pass", NULL };
@@ -76,14 +76,22 @@ static const char *miccmd[]     = { "pactl", "set-source-mute", "@DEFAULT_SOURCE
 static const char *brightup[]   = { "brightnessctl", "set", "+10", NULL };
 static const char *brightdown[] = { "brightnessctl", "set", "10-", NULL };
 static const char *slock[]      = { "slock", "systemctl", "suspend", "-i", NULL };
+static const char *screen_1[]   = { "screen_1.sh", NULL };
+static const char *screen_2[]   = { "screen_2.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	/* MODKEY */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd} },	
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_Print,  spawn,          {.v = screenshot} },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = screen_1} },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = screen_2} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock} },
+	{ MODKEY|ControlMask,           XK_p,      spawn,          {.v = passcmd } },
+	{ MODKEY|ControlMask,           XK_r,      spawn,          {.v = rofibeats } },
+	{ MODKEY|ControlMask,           XK_m,      spawn,          {.v = mixer} },
+	{ MODKEY|ControlMask,           XK_n,      spawn,          {.v = rss} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -91,32 +99,20 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = filecmd } },
-	{ MODKEY|ShiftMask,             XK_m,      zoom,           {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock} },
-	{ MODKEY|ControlMask,           XK_p,      spawn,          {.v = passcmd } },
-	{ MODKEY|ControlMask,           XK_r,      spawn,          {.v = rofibeats } },
-	{ MODKEY|ControlMask,           XK_m,      spawn,          {.v = mixer} },
-	{ 0,             XF86XK_AudioLowerVolume,  spawn,          {.v = voldowncmd } },
-	{ 0,             XF86XK_AudioMute,         spawn,          {.v = mutecmd } },
-	{ 0,             XF86XK_AudioRaiseVolume,  spawn,          {.v = volupcmd   } },
-	{ 0,             XF86XK_AudioMicMute,      spawn,          {.v = miccmd } },
-	{ 0,             XF86XK_MonBrightnessUp,   spawn,          {.v = brightup} },
-	{ 0,             XF86XK_MonBrightnessDown, spawn,          {.v = brightdown} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -126,6 +122,13 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,             XF86XK_AudioLowerVolume,  spawn,          {.v = voldowncmd } },
+	{ 0,             XF86XK_AudioMute,         spawn,          {.v = mutecmd } },
+	{ 0,             XF86XK_AudioRaiseVolume,  spawn,          {.v = volupcmd   } },
+	{ 0,             XF86XK_AudioMicMute,      spawn,          {.v = miccmd } },
+	{ 0,             XF86XK_MonBrightnessUp,   spawn,          {.v = brightup} },
+	{ 0,             XF86XK_MonBrightnessDown, spawn,          {.v = brightdown} },
 };
 
 /* button definitions */
