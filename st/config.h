2df1c31383c31b6f02594386fa417a5e1cf0c1d1
Marko Kriska <marko.kriska@pm.me>
2023-01-24 00:59:10 +0100

    update

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:style=Retina:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Fira Code:style=Retina:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
