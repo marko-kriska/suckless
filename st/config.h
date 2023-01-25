134595b88161a1ea43e1fa54e666b20ca250e36a
Marko Kriska <marko.kriska@pm.me>
2023-01-25 17:15:09 +0100

    update

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 10;
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
