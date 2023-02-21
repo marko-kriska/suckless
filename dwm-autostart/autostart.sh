bf38c512cb7f916886bd1626c568341e59170e28
Marko Kriška <95656575+marko-kriska@users.noreply.github.com>
2023-02-21 20:57:30 +0100

    Update autostart.sh

--- dwm-autostart/autostart.sh
+++ dwm-autostart/autostart.sh
@@ -6,5 +6,5 @@ done &
 
 # Autostart section
 xrandr --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
-xrandr --output HDMI2 --set "Broadcast RGB" "Full"
+xrandr --output HDMI-2 --set "Broadcast RGB" "Full"
 xbanish &
