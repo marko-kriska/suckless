427a30000334575da6a2daa2f9e581ec756a39c0
Marko Kriska <marko.kriska@pm.me>
2023-02-21 18:23:00 +0100

    update

--- dwm-autostart/autostart.sh
+++ dwm-autostart/autostart.sh
@@ -1,4 +1,4 @@
 # Autostart section
-xrandr --output eDP1 --mode 1366x768 --pos 1920x0 --output HDMI2 --primary --mode 1920x1080 --pos 0x0
+xrandr --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 xrandr --output HDMI2 --set "Broadcast RGB" "Full"
 xbanish &
