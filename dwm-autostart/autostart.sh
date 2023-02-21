2f155edaa2c1ced9d939d2090d29602c6a7b0112
Marko Kriska <marko.kriska@pm.me>
2023-02-21 20:36:36 +0100

    update

--- dwm-autostart/autostart.sh
+++ dwm-autostart/autostart.sh
@@ -1,3 +1,9 @@
+# Statusbar loop
+while true; do
+   xsetroot -name " $( date +"%F %R" )"
+   sleep 1m    # Update time every minute
+done & 
+
 # Autostart section
 xrandr --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
 xrandr --output HDMI2 --set "Broadcast RGB" "Full"
