0f3da6a528ae6056aeca54e895d11c6126855066
Marko Kriska <marko.kriska@pm.me>
2023-02-23 17:29:26 +0100

    update

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM Plex Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
