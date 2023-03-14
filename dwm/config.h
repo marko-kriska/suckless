0376b0d3f82f88369ccf2ecc8b814d958b306bce
Marko Kriska <marko.kriska@pm.me>
2023-03-14 19:13:39 +0100

    update

--- dwm/config.h
+++ dwm/config.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray1, "-sf", col_cyan, NULL };
-static const char *roficmd[]    = { "rofi -show drun", NULL };
+static const char *roficmd[]    = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]    = { "alacritty", NULL };
 static const char *mixer[]      = { "alacritty", "--class", "pulsemixer,pulsemixer", "-e", "pulsemixer", NULL };
 static const char *rss[]        = { "alacritty", "--class", "newsboat,newsboat", "-e", "newsboat", NULL };
