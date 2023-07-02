4e47676ec5ebd4d1bd2c4bb0df5dfd197fdc3bfe
Marko Kriška <marko.kriska@pm.me>
2023-07-02 17:56:36 +0200

    update

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=13";
+static char *font = "JetBrains Mono:pixelsize=14";
 static int borderpx = 20;
 
 /*
